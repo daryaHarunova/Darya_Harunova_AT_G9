9d5e604cba78a3a2442b4d9f4cb2a285f03d06a9
Darya Harunova <darya.harunova@gmail.com>
2024-02-13 10:02:27 +0100

    second task (array)

---
+++ src/homework/day4/ArrayDrawing.java
@@ -0,0 +1,22 @@
+package homework.day4;
+//написать метод, который вернет сумму тех элементов целочисленного массива,
+// которые кратны числу текущего месяца (массив подается в сигнатуру метода)
+
+
+public class ArrayDrawing {
+    public void draw() {
+            System.out.println("   ^__^");
+            System.out.println("   (oo)\\_______");
+            System.out.println("   (__)\") \\/\\  \\");
+            System.out.println("       ||----w |\\");
+            System.out.println("       ||     ||");
+        }
+    public static void main(String[] args) {
+        ArrayDrawing newArrayDrawing = new ArrayDrawing();
+        newArrayDrawing.draw();
+        }
+    }
+
+
+
+
