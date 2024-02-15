2b72d133b281e285096bd5e6cb75eda9ffe2cd73
Darya Harunova <darya.harunova@gmail.com>
2024-02-15 17:42:03 +0100

    create task 2

--- src/homework/day5/playground/essence/creatures/plant/flowers/Chamomile.java
+++ src/homework/day5/playground/essence/creatures/plant/flowers/Chamomile.java
@@ -1,7 +1,7 @@
 package homework.day5.playground.essence.creatures.plant.flowers;
 
 public class Chamomile extends Flowers {
-    public Chamomile(double weight, int mass, double weight1, String name) {
-        super(weight, mass, weight1, name);
+    public Chamomile(double weight, int mass, String name) {
+        super(weight, mass, name);
     }
 }
--- src/homework/day5/playground/essence/creatures/plant/vegetable/Carrot.java
+++ src/homework/day5/playground/essence/creatures/plant/vegetable/Carrot.java
@@ -2,7 +2,7 @@ package homework.day5.playground.essence.creatures.plant.vegetable;
 
 
 public class Carrot extends Vegetable {
-    public Carrot(double weight, int mass, double weight1, String name) {
-        super(weight, mass, weight1, name);
+    public Carrot(double weight, int mass, String name) {
+        super(weight, mass, name);
     }
 }
