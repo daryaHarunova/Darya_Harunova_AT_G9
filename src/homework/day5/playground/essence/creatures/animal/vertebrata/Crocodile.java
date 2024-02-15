a0dba3d40264bf3b9279719bfdccdbf23459d82c
Darya Harunova <darya.harunova@gmail.com>
2024-02-15 18:13:01 +0100

    create task 3

--- src/homework/day5/playground/essence/creatures/animal/vertebrata/Crocodile.java
+++ src/homework/day5/playground/essence/creatures/animal/vertebrata/Crocodile.java
@@ -1,6 +1,8 @@
 package homework.day5.playground.essence.creatures.animal.vertebrata;
 
-public class Crocodile extends Vertebrata {
+import homework.day5.playground.essence.creatures.Crawlable;
+
+public class Crocodile extends Vertebrata implements Crawlable {
     public Crocodile(double weight, int mass, String name) {
         super(weight, mass, name);
     }
