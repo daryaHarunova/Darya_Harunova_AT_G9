81313dda59652ed60568180337124ef3acdc5b3f
Darya Harunova <darya.harunova@gmail.com>
2024-02-29 15:29:26 +0100

    stream task1

--- src/homework/day9/ButterfliesRunner.java
+++ src/homework/day9/ButterfliesRunner.java
@@ -2,7 +2,6 @@ package homework.day9;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ButterfliesRunner {
     public static void main(String[] args) {
@@ -14,21 +13,8 @@ public class ButterfliesRunner {
 
         //Добавить к каждому элементу кавычки
 
-        butterflies = butterflies.stream().map(s -> "\"" + s + "\"").collect(Collectors.toList());
-
-        System.out.println(butterflies);
-
-        //Оставить только те слова, которые содержат букву "a" и букву "о"
-
-        butterflies = butterflies.stream().filter(s -> s.contains("a") && s.contains("o")).collect(Collectors.toList());
-
-        System.out.println(butterflies);
-
-        //Вывести слова в консоль с новой строки
-
-        butterflies.forEach(System.out::println);
-
+        butterflies.stream().map(s -> "\"" + s + "\"")
+                .filter(s -> s.contains("a") && s.contains("o"))
+                .forEach(System.out::println);
     }
-
-
 }
